6cb5880d9d71258db56e05429b8bbed84b81a07c
lingardd09 <sherlockicker@gmail.com>
2026-05-05 15:40:24 +0900

    feat: Add landing page, styles, and Google login
    Add a new LandingPage component and full-page styles, include Google fonts, and redesign the Google sign-in button. Created src/pages/css/LandingPage.css and updated LandingPage.tsx to assemble the landing UI; refactored GoogleLoginButton markup and moved/renamed its stylesheet (components/CSS -> components/css) with new visual styles. Simplified App.css to global base styles and updated App.tsx imports and JSX (router wrapped in a .counter div). Also updated index.html to preload Google Fonts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
-import LandingPage from "./pages/LandingPage"
-import GoogleLoginButton from "./components/GoogleLoginButton"
+import LandingPage from "./pages/LandingPage";
+import GoogleLoginButton from "./components/GoogleLoginButton";
 
 function App() {
-
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/googleloginbutton" element={<GoogleLoginButton />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="counter">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/googleloginbutton" element={<GoogleLoginButton />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
